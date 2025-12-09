3cd27c7a25cc09a1ca8e87c09e61ecb7234be555
Minseo Kim(Kelly) <kimm040@newschool.edu>
2025-12-09 17:14:47 -0500

    final

--- grainy/script.js
+++ grainy/script.js
@@ -1,14 +1,11 @@
 document.addEventListener("mousemove", (event) => {
-
-
   const count = Math.floor(Math.random() * 4) + 2;
 
   for (let i = 0; i < count; i++) {
     const dot = document.createElement("div");
     dot.classList.add("grain-dot");
 
-   
-    const offsetX = (Math.random() - 0.5) * 200; 
+    const offsetX = (Math.random() - 0.5) * 200;
     const offsetY = (Math.random() - 0.5) * 100;
 
     dot.style.left = event.clientX + offsetX + "px";
